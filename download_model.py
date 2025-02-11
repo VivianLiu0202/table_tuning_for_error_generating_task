b5ffe7f9542c0e2eaa2cf70e53f2b87f8aa1d8a3
LiuVivian <93787365+VivianLiu0202@users.noreply.github.com>
2025-02-11 21:20:22 +0800

    Delete download_model.py

--- download_model.py
+++
@@ -1,5 +0,0 @@
-from huggingface_hub import snapshot_download
-import os
-os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
-
-snapshot_download(repo_id="meta-llama/Meta-Llama-3.1-8B", local_dir="./llama3.1-8B", token="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")
